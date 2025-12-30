a79de3e65a69f7cf6051f8da4869dbc874d333f8
KanonKC <kanonkce@gmail.com>
2025-12-30 20:36:28 +0700

    Using cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,11 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			out: 'build',
-			precompress: false,
-			envPrefix: '',
-			polyfill: true,
-		}),
-		alias: {
-			"@/*": "./src/lib/*",
-		},
+		adapter: adapter(),
 	}
 };
 
